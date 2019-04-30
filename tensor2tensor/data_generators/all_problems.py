5aa2d81468ae6c03ddf9ad679fe3d19db6781ee2
Alexander Bezzubov <bzz@apache.org>
2019-04-30 09:53:03 +0200

    method name: add python doc string v2 dataset
    Dataset: https://github.com/EdinburghNLP/code-docstring-corpus/tree/master/V2
    Paper: https://arxiv.org/abs/1707.02275
    
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- tensor2tensor/data_generators/all_problems.py
+++ tensor2tensor/data_generators/all_problems.py
@@ -92,42 +92,44 @@ MODULES = [
     "tensor2tensor.data_generators.wikitext103",
     "tensor2tensor.data_generators.wsj_parsing",
     "tensor2tensor.data_generators.wnli",
+    "tensor2tensor.data_generators.yelp_polarity",
+    "tensor2tensor.data_generators.yelp_full",
     "tensor2tensor.envs.mujoco_problems",
     "tensor2tensor.envs.tic_tac_toe_env_problem",
+    "tensor2tensor.data_generators.method_name",
 ]
 ALL_MODULES = list(MODULES)
 
 
-
 def _is_import_err_msg(err_str, module):
-  parts = module.split(".")
-  suffixes = [".".join(parts[i:]) for i in range(len(parts))]
-  return err_str in (
-      ["No module named %s" % suffix for suffix in suffixes] +
-      ["No module named '%s'" % suffix for suffix in suffixes])
+    parts = module.split(".")
+    suffixes = [".".join(parts[i:]) for i in range(len(parts))]
+    return err_str in (
+        ["No module named %s" % suffix for suffix in suffixes] +
+        ["No module named '%s'" % suffix for suffix in suffixes])
 
 
 def _handle_errors(errors):
-  """Log out and possibly reraise errors during import."""
-  if not errors:
-    return
-  log_all = True  # pylint: disable=unused-variable
-  err_msg = "T2T: skipped importing {num_missing} data_generators modules."
-  print(err_msg.format(num_missing=len(errors)))
-  for module, err in errors:
-    err_str = str(err)
-    if not _is_import_err_msg(err_str, module):
-      print("From module %s" % module)
-      raise err
-    if log_all:
-      print("Did not import module: %s; Cause: %s" % (module, err_str))
+    """Log out and possibly reraise errors during import."""
+    if not errors:
+        return
+    log_all = True  # pylint: disable=unused-variable
+    err_msg = "T2T: skipped importing {num_missing} data_generators modules."
+    print(err_msg.format(num_missing=len(errors)))
+    for module, err in errors:
+        err_str = str(err)
+        if not _is_import_err_msg(err_str, module):
+            print("From module %s" % module)
+            raise err
+        if log_all:
+            print("Did not import module: %s; Cause: %s" % (module, err_str))
 
 
 def import_modules(modules):
-  errors = []
-  for module in modules:
-    try:
-      importlib.import_module(module)
-    except ImportError as error:
-      errors.append((module, error))
-  _handle_errors(errors)
+    errors = []
+    for module in modules:
+        try:
+            importlib.import_module(module)
+        except ImportError as error:
+            errors.append((module, error))
+    _handle_errors(errors)
